ba0f536d79e9e9e1765d64a2238b703aa433c190
Ohimata BN <benjaminohiomatanathaniel@gmail.com>
2025-01-05 10:59:37 +0100

    Fixed the temperature output to 1

--- script.js
+++ script.js
@@ -7,12 +7,12 @@ let temp;
 function convert() {
   if (toFahrenheit.checked) {
     temp = Number(textBox.value);
-    temp = temp * 9 / 5 * 32;
-    result.innerHTML = `${temp} &degF`;
+    temp = temp * 9 / 5 + 32;
+    result.innerHTML = `${temp.toFixed(1)} &degF`;
   } else if (toCelsius.checked) {
     temp = Number(textBox.value);
-    temp = temp / 9 * 5 / 32;
-    result.innerHTML = `${temp} &degC`;
+    temp = (temp - 32) * (5 / 9);
+    result.innerHTML = `${temp.toFixed(1)} &degC`;
   }
   else {
     result.textContent = "Select a unit"
